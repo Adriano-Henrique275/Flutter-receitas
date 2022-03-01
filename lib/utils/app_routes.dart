e3ab0d2426bca9794379c9f04df98a69df59c02d
Adriano Henrique <adriano.henrique275@gmail.com>
2022-03-01 19:55:47 -0300

    meu projeto meals

---
+++ lib/utils/app_routes.dart
@@ -0,0 +1,6 @@
+class AppRoutes {
+  static const HOME = '/';
+  static const CATEGORIES_MEALS = '/catedories-meals';
+  static const MEALS_DETAIL = '/meals-detail';
+  static const SETTINGS = '/settings';
+}
